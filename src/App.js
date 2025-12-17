7991ce9b40f454c7353c4e0fc41d8101c6b12e99
Ganesh Gunda <ganeshhgunda123@gmail.com>
2025-12-18 01:35:09 +0530

    Update Axiom V2 date from 22 Dec to 31 Dec

--- src/App.js
+++ src/App.js
@@ -343,7 +343,7 @@ function App() {
                         tooltip.classList.remove('tooltip-fade');
                       }}
                    >
-                     Axiom V2 running from 8 Dec to 22 Dec
+                     Axiom V2 running from 8 Dec to 31 Dec
                    </span>
                  </p>
                  <h3><a href="https://hackatime.hackclub.com/" style={{color: 'lightblue'}}>Hackatime</a> time tracking is required.</h3>
